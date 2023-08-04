d64277aa171c18799d301eecd6907e7969eb65ba
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-08-04 12:43:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# interactive-rating-z
+ Frontend Mentor - Newbie 3
---
+++ app.js
@@ -0,0 +1,23 @@
+const rating = document.querySelector('.wrapper-rating');
+const btn = document.querySelector('.btn');
+const ratingBtns = document.querySelectorAll('.rate-btn')
+const score = document.querySelector(".thank__text__score");
+const ratingMsg = document.getElementById('rate-msg')
+
+btn.addEventListener('click', () => {
+    rating.classList.add('rating--sh');
+    if (!document.querySelector(".rate-btn.checked")) {
+      score.innerHTML = "No rating?";
+    }
+});
+
+ratingBtns.forEach((ratingBtn) => {
+    ratingBtn.addEventListener("click", () => {
+        document.querySelector('.checked')?.classList.remove('checked');
+        ratingBtn.classList.add('checked');
+        ratingMsg.innerHTML = ratingBtn.innerHTML;
+      });
+})
+    
+
+
