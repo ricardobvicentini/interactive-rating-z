0e63f749462e037683de362b0d00d12d84e718c8
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-08-04 14:04:08 -0300

    Adjust translateZ

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const rating = document.querySelector('.wrapper-rating');
 const btn = document.querySelector('.btn');
 const ratingBtns = document.querySelectorAll('.rate-btn')
 const score = document.querySelector(".thank__text__score");
-const ratingMsg = document.getElementById('rate-msg')
+const ratingMsg = document.getElementById('rate-msg');
 
 btn.addEventListener('click', () => {
-    rating.classList.add('rating--sh');
-    if (!document.querySelector(".rate-btn.checked")) {
-      score.innerHTML = "No rating?";
+    rating.classList.add("rating--sh");
+    if (!document.querySelector('.rate-btn.checked')) {
+      score.innerHTML = 'No rating?';
     }
 });
 
